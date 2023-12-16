33936797d234a5c8043d374a6cd1cf4af9499277
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2023-12-16 23:22:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+//app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.post('/chart', async (req, res) => {
+    const { chartType, year, yod } = req.body;
+  
+    // Create the Chart.js URL
+    const chartUrl = `https://quickchart.io/chart?c={type:'${chartType}',data:{labels:[${year}],datasets:[{label:'Users',data:[${yod}]}]}}`;
+    console.log(chartUrl);
+  
+    // Fetch the modified URL
+    try {
+      const response = await axios.get(chartUrl);
+      res.render('chart.ejs', { chartUrl });
+    } catch (error) {
+      console.error('Error fetching chart data:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
